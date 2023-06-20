c2ecf060268197f0b006f518291f5296b914e6b8
lrgsouza <lucas_souza@outlook.com>
2023-06-19 21:01:01 -0300

    [19-06-2023] MVC

---
+++ workspace_eclipse/ADS_Padrao_MVC/src/main/java/br/inatel/labs/labads/padraomvc/model/service/ProdutoService.java
@@ -0,0 +1,30 @@
+package br.inatel.labs.labads.padraomvc.model.service;
+import br.inatel.labs.labads.padraomvc.model.entidade.*;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import jakarta.annotation.PostConstruct;
+
+@Service
+public class ProdutoService {
+	private List<Produto> listaDeProdutos = new ArrayList<>();
+	
+	@PostConstruct
+	private void setup() {
+		Produto p1 = new Produto(1L, "Furadeira", 300.00);
+		Produto p2 = new Produto(2L, "Lixadeira", 200.00);
+		Produto p3 = new Produto(3L, "Serra Circular", 500.00);
+		
+		listaDeProdutos.add(p1);
+		listaDeProdutos.add(p2);
+		listaDeProdutos.add(p3);
+	}
+	
+	public List<Produto> findAll(){
+		return listaDeProdutos;
+	}
+	
+	
+}
